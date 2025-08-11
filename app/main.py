53d7737a7647d2df56987003b5bc31f718ef3900
Pushpender Singh <rishu7691@gmail.com>
2025-08-11 13:44:28 +0530

    workflow changes

--- app/main.py
+++ app/main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello from Flask CI/CD, rollling image policy new"
+    
+@app.route('/health')
+def health():
+    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
