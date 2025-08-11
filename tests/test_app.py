eb6eb1f8f7a4dac7f84bfd8389505d5203789c86
Pushpender Singh <rishu7691@gmail.com>
2025-08-11 18:14:23 +0530

    new unstable version

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ def test_homepage():
     client = app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello from Flask CI/CD, rolllng image policy new" in response.data
+    assert b"Hello from Flask CI/CD, rolling image policy new" in response.data
