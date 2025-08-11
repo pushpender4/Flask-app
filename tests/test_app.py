c09dc051f6e12bb9918712cbdcbf0412b9155857
Pushpender Singh <rishu7691@gmail.com>
2025-08-11 13:14:26 +0530

    test case added

---
+++ tests/test_app.py
@@ -0,0 +1,7 @@
+from app import app
+
+def test_homepage():
+    client = app.test_client()
+    response = client.get("/")
+    assert response.status_code == 200
+    assert b"Hello from Flask CI/CD, rollling image policy" in response.data
