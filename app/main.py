7b191bcec02e5269de3b500c67577ce048bf9401
Pushpender Singh <rishu7691@gmail.com>
2025-08-11 19:09:32 +0530

    version4

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask CI/CD, this is a unhealthy version2"
+    return "Hello from Flask CI/CD, this is a unhealthy versio4"
 
 # @app.route('/health')
 # def health():
