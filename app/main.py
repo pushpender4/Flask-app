7cc7e9ef45664e896d04f5da544c33ac7833d38b
Pushpender Singh <rishu7691@gmail.com>
2025-08-15 12:51:07 +0530

    fail test 1

--- app/main.py
+++ app/main.py
@@ -6,13 +6,13 @@ app = Flask(__name__)
 def hello():
     return "Hello from Flask CI/CD, this is a healthy version 11"
 
-@app.route('/health')
-def health():
-    return "OK", 200
-
-# @app.route("/health")
+# @app.route('/health')
 # def health():
-#     return "FAIL", 500
+#     return "OK", 200
+
+@app.route("/health")
+def health():
+    return "FAIL", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
