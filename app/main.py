a8e06441e07f137bb5867dfbad7c0e1bfe91597d
Pushpender Singh <rishu7691@gmail.com>
2025-08-15 13:13:10 +0530

    v3 lkg update

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Flask CI/CD, this is a healthy version to push to lkg v2"
+    return "Hello from Flask CI/CD, this is a healthy version to push to lkg v3"
 
 @app.route('/health')
 def health():
